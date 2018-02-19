23fdaa92ecb92706b89ff77783845603ae880e9f
KetkiMahajan <mahajanketki24@gmail.com>
2018-02-19 15:06:50 -0600

    Fibonacci sequence using loop

---
+++ Data Structure/src/FibWithLI.java
@@ -0,0 +1,22 @@
+//ketki mahajan
+//Fibonacci using loop invariant
+public class FibWithLI {
+    public static int fib(int n) {
+        int[] temp = new int[n + 1];
+        if (n == 0) {
+            temp[0] = 0;
+        }
+        if (n == 1) {
+            temp[1] = 1;
+        } else {
+            for (int i = n; i > 1; i--) {
+                temp[i] = fib(n - 1) + fib(n - 2);
+            }
+        }
+        return temp[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(3));
+    }
+}
