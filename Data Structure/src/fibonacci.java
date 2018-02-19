32a97870322dc50ba8f5f302b9f87e36857cfa33
KetkiMahajan <mahajanketki24@gmail.com>
2018-02-19 14:54:10 -0600

    Fibonacci sequence using recursion

---
+++ Data Structure/src/fibonacci.java
@@ -0,0 +1,19 @@
+//ketki mahajan
+//Fibonacci using recursion
+public class fibonacci {
+    public static int fib(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(9));
+    }
+}
